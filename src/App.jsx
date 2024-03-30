e215efcdba675b1d375aa2d9a59560c1de74228e
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-03-30 21:53:58 +1300

    updated the content in the app

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ const App = () => {
 	return (
 		<main className='mx-auto grid grid-cols-1 place-items-center min-h-screen'>
 			<div className='card w-96 bg-base-100 shadow-xl'>
-				<figure className='px-10 pt-10'>
-					<img
-						src='https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg'
-						alt='Shoes'
-						className='rounded-xl'
-					/>
-				</figure>
+				<header className=' grid place-items-center rounded'>
+					<h1 className='capitalize text-2xl'>your Result</h1>
+					<div className='w-36 h-36 rounded-full bg-blue-600 flex flex-col align-center justify-center'>
+						<h2 className='capitalize text-5xl text-center'>76</h2>
+						<span className='text-lg text-center'>of 100</span>
+					</div>
+					<h3 className='card-title'>Great!</h3>
+					<p>
+						You scored higher than 65% of the people who have taken these tests.
+					</p>
+				</header>
 				<div className='card-body items-center text-center'>
-					<h2 className='card-title'>Shoes!</h2>
-					<p>If a dog chews shoes whose shoes does he choose?</p>
 					<div className='card-actions'>
 						<button className='btn btn-primary'>Buy Now</button>
 					</div>
