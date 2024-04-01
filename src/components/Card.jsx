47092736ac8528ce49ca0a7d5b53ae5b61e9442c
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-04-01 19:01:08 +1300

    added a bg color to the main card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import { CardBody, CardHeader } from './index';
 const Card = () => {
 	return (
 		<main className='mx-auto grid grid-cols-1 place-items-center min-h-screen'>
-			<section className='w-full max-w-[375px] md:max-w-[736px] flex flex-col justify-center items-center '>
+			<section className='bg-white w-full max-w-[375px] md:max-w-[736px] flex flex-col justify-center items-center'>
 				<CardHeader />
 				<CardBody />
 			</section>
