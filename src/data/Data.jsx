f2ea7e3ddc5e826da3331039e628a65a0605c485
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-04-01 23:27:49 +1300

    added colors to the data

--- src/data/Data.jsx
+++ src/data/Data.jsx
@@ -8,21 +8,29 @@ const data = [
 		category: 'Reaction',
 		score: 80,
 		icon: reaction,
+		colorFrom: 'rgba(255, 255, 255, 0.95)',
+		colorTo: '#F55',
 	},
 	{
 		category: 'Memory',
 		score: 92,
 		icon: memory,
+		colorFrom: 'rgba(255, 255, 255, 0.95)',
+		colorTo: '#FFB21E',
 	},
 	{
 		category: 'Verbal',
 		score: 61,
 		icon: verbal,
+		colorFrom: 'rgba(255, 255, 255, 0.95)',
+		colorTo: '#00BB8F',
 	},
 	{
 		category: 'Visual',
 		score: 72,
 		icon: visual,
+		colorFrom: 'rgba(255, 255, 255, 0.95)',
+		colorTo: '#1125D6',
 	},
 ];
 
