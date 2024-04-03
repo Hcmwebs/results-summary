b5e0b3687534d616ebb237aeefc0f29a2746701b
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-04-04 01:59:05 +1300

    added Single Item as indicated

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,5 +1,6 @@
 import Card from './Card';
 import CardBody from './CardBody';
 import CardHeader from './CardHeader';
+import SingleItem from './SingleItem';
 
-export { Card, CardHeader, CardBody };
+export { Card, CardHeader, SingleItem, CardBody };
