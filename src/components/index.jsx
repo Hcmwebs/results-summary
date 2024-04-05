896817ff41d414d46f5bc0779dca833f0a56755a
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-04-05 21:32:00 +1300

    indexed a component

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,5 +2,6 @@ import Card from './Card';
 import CardBody from './CardBody';
 import CardHeader from './CardHeader';
 import SingleItem from './SingleItem';
+import Button from './Button';
 
-export { Card, CardHeader, SingleItem, CardBody };
+export { Card, CardHeader, SingleItem, CardBody, Button };
