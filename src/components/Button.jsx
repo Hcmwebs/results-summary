0ce48f5869871ed060067a593d6b2b04e4809999
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-04-05 21:31:32 +1300

    added, indexed and rendered the button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = () => {
+	return (
+		<button className='btn btn-primary bg-black p-8 grid grid-cols-1 place-items-center hover:bg-blue-600 text-white rounded-lg'>
+			Continue
+		</button>
+	);
+};
+
+export default Button;
