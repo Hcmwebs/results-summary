ea0125631a75f2045ae02883546b423d7fcb3748
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-04-06 23:47:54 +1300

    set the button as a block element

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = () => {
 	return (
-		<button className='btn btn-primary w-[92%] bg-black p-8 grid grid-cols-1 place-items-center hover:bg-blue-600 text-white rounded-full'>
+		<button className='btn btn-primary btn-block grid grid-cols-1 place-items-center hover:bg-blue-600 text-white rounded-full'>
 			Continue
 		</button>
 	);
