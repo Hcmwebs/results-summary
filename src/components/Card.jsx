1917b3d0cc4c7f8a6fa5b962711354669ad2a9c9
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-04-06 23:34:33 +1300

    moved the button to the main Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,12 @@
-import { CardBody, CardHeader } from './index';
+import { Button, CardBody, CardHeader } from './index';
 
 const Card = () => {
 	return (
 		<main className='mx-auto grid grid-cols-1 place-items-center min-h-screen'>
-			<section className='bg-white w-full max-w-[375px] md:max-w-[736px] flex flex-col justify-center items-center'>
+			<section className='bg-white w-full max-w-[375px] md:max-w-[736px] flex flex-col justify-center items-center pb-8'>
 				<CardHeader />
 				<CardBody />
+				<Button />
 			</section>
 		</main>
 	);
