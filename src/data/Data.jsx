7491ce61d7e619ec6ba76df5df8803adfd12e376
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-04-08 23:19:24 +1200

    updated the data

--- src/data/Data.jsx
+++ src/data/Data.jsx
@@ -8,8 +8,6 @@ const data = [
 		category: 'Reaction',
 		score: 80,
 		icon: reaction,
-		color: 'hsl(0,89%,65%)',
-		bgColor: 'hsla(0,89%,65%,0.15)',
 		className:
 			'flex w-full justify-between align-center p-6 shadow-lg rounded-lg text-[hsl(0,89%,65%)] bg-[hsla(0,89%,65%,0.15)]',
 	},
@@ -17,8 +15,6 @@ const data = [
 		category: 'Memory',
 		score: 92,
 		icon: memory,
-		color: 'hsl(39, 100%, 56%)',
-		bgColor: 'hsla(39,100%,56%,0.15)',
 		className:
 			'flex w-full justify-between align-center p-6 shadow-lg rounded-lg text-[hsl(39,100%,56%)] bg-[hsla(39,100%,56%,0.15)]',
 	},
@@ -26,17 +22,13 @@ const data = [
 		category: 'Verbal',
 		score: 61,
 		icon: verbal,
-		color: 'hsl(166, 100%, 37%)',
-		bgColor: 'hsla(166,100%,37%,0.15)',
 		className:
-			'flex w-full justify-between align-center p-6 shadow-lg rounded-lg text-[hsl(166, 100%, 37%)] bg-[hsla(166,100%,37%,0.15)]',
+			'flex w-full justify-between align-center p-6 shadow-lg rounded-lg text-[hsl(166,100%,37%)] bg-[hsla(166,100%,37%,0.15)]',
 	},
 	{
 		category: 'Visual',
 		score: 72,
 		icon: visual,
-		color: 'hsla(234, 85%, 45%,1.0)',
-		bgColor: 'hsla(234,85%,45%,0.15)',
 		className:
 			'flex w-full justify-between align-center p-6 shadow-lg rounded-lg text-[hsl(234,85%,45%)] bg-[hsla(234,65%,45%,0.15)]',
 	},
