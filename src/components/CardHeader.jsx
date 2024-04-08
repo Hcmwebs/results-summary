8a9aac3a80a44b1137259745e3f12c5389523a69
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-04-09 00:55:06 +1200

    added bg gradient to the header

--- src/components/CardHeader.jsx
+++ src/components/CardHeader.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-
 const CardHeader = () => {
 	return (
 		<>
-			<header className='bg-blue-500 w-full grid grid-cols-1 place-items-center gap-y-6 py-8 rounded-b-[2rem] '>
-				<h1 className='text-center'>Your Results</h1>
+			<header className='bg-gradient-to-b from-[hsla(252,100%,67%,1)] to-[hsla(241,81%,54%,1)] 100% w-full grid grid-cols-1 place-items-center gap-y-6 py-8 rounded-b-[2rem] '>
+				<h1 className='text-center text-[#cac9ff] font-bold text-lg'>
+					Your Results
+				</h1>
 				<div className='w-36 h-36 rounded-full flex flex-col justify-center items-center bg-blue-800'>
 					<h2>76</h2>
 					<p>of 100</p>
