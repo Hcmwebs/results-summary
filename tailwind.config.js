7860733792de2ea7caab958089c3704388dde2d8
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-04-09 00:14:06 +1200

    added the color themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,20 @@ export default {
 		extend: {},
 	},
 	plugins: [require('@tailwindcss/typography'), require('daisyui')],
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: 'hsla(252,100%,67%,1)',
+					secondary: 'hsla(241,81%,54%,1)',
+					accent: '#37cdbe',
+					neutral: 'hsla(241,100%,89%,1)',
+					'base-100': 'hsla(0,0%,100%,1)',
+					black: 'hsla(223,30%,27,1)',
+				},
+			},
+			'dark',
+			'cupcake',
+		],
+	},
 };
