f56ca1c00c9863fb2f6f1f31f92b70a6fbd4fe8b
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-04-10 23:52:55 +1200

    completed the styles for the mobiles  devices

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Button = () => {
 	return (
-		<button className='btn btn-primary btn-block grid grid-cols-1 place-items-center hover:bg-blue-600 text-white rounded-full'>
-			Continue
-		</button>
+		<>
+			<button className='min-h-14 btn btn-[#303B59] btn-block grid grid-cols-1 place-items-center hover:bg-gradient-to-b from-[hsla(252,100%,67%,1)] to-[hsla(241,81%,54%,1)] 100% text-white text-lg font-normal rounded-full'>
+				Continue
+			</button>
+		</>
 	);
 };
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import { CardBody, CardFooter, CardHeader } from './index';
 const Card = () => {
 	return (
 		<main className='mx-auto grid grid-cols-1 place-items-center min-h-screen'>
-			<section className='bg-white w-full max-w-[375px] md:max-w-[736px] flex flex-col justify-center items-center pb-8'>
+			<section className='bg-white w-full max-w-[375px] md:max-w-[736px] flex flex-col justify-center items-center pb-8 gap-y-6'>
 				<CardHeader />
 				<CardBody />
 				<CardFooter />
