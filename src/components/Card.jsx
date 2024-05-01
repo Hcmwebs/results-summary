cc0ebe0632f39acd8cfd70fe8b4ba3f9d95225bc
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-05-01 16:55:09 +1200

    set up the height the card header

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import { CardBody, CardHeader } from './index';
 const Card = () => {
 	return (
 		<main className='mx-auto grid grid-cols-1 place-items-center min-h-screen'>
-			<section className='bg-white w-full max-w-[375px] md:max-w-[736px] flex flex-col md:flex-row justify-center items-center pb-8 md:py-0 gap-y-6'>
+			<section className='bg-white w-full h-full max-w-[375px] md:max-w-[736px] md:max-h-[512px] flex flex-col md:flex-row justify-center items-center pb-8 md:py-0 gap-y-6'>
 				<CardHeader />
 				<CardBody />
 			</section>
