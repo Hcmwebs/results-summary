6a06c1a74ba6823f1a304e0292b421e45d75436b
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-05-02 00:51:53 +1200

    adjusted th font sizes for the corresponding screens

--- src/components/CardHeader.jsx
+++ src/components/CardHeader.jsx
@@ -2,14 +2,14 @@ const CardHeader = () => {
 	return (
 		<>
 			<header className='bg-gradient-to-b from-[hsla(252,100%,67%,1)] to-[hsla(241,81%,54%,1)] 100% w-full md:h-full grid grid-cols-1 place-items-center gap-y-6 py-8 rounded-b-[2rem] md:rounded-[2rem] '>
-				<h1 className='para-text font-bold'>Your Results</h1>
+				<h1 className='para-text font-bold text-xl md:text-2xl'>Your Result</h1>
 				<div className='w-36 h-36 rounded-full flex flex-col justify-center items-center bg-gradient-to-b from-[#4D21C9] to-[rgba(37,33,201,0.00)] 100%'>
-					<h2 className='text-[56px] font-bold text-white'>76</h2>
+					<h2 className='text-5xl md:text-7xl font-extrabold text-white'>76</h2>
 					<p className='para-text'>of 100</p>
 				</div>
 				<div className='grid grid-cols-1 place-items-center gap-y-2'>
-					<h3 className='text-2xl capitalize text-white'>great</h3>
-					<p className='para-text px-14 font-normal text-base'>
+					<h3 className='text-2xl md:text-3xl capitalize text-white'>great</h3>
+					<p className='para-text px-14 font-medium text-base md:text-lg'>
 						You scored higher than 65% of the people who have taken these tests.
 					</p>
 				</div>
