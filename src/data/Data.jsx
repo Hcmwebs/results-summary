c904cb8835919537067c758aad52a9cbbbc716ca
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-05-01 18:01:35 +1200

    added hover and transition classes to the cards

--- src/data/Data.jsx
+++ src/data/Data.jsx
@@ -9,28 +9,28 @@ const data = [
 		score: 80,
 		icon: reaction,
 		className:
-			'flex w-full justify-between align-center p-6 shadow-lg hover:shadow-xl rounded-lg text-[hsl(0,89%,65%)] bg-[hsla(0,89%,65%,0.15)] ',
+			'flex w-full justify-between align-center p-6 shadow-lg hover:shadow-xl rounded-lg text-[hsl(0,89%,65%)] bg-[hsla(0,89%,65%,0.15)] hover:scale-[0.98] transition duration-500 ease-in-out',
 	},
 	{
 		category: 'Memory',
 		score: 92,
 		icon: memory,
 		className:
-			'flex w-full justify-between align-center p-6 shadow-lg hover:shadow-xl rounded-lg text-[hsl(39,100%,56%)] bg-[hsla(39,100%,56%,0.15)]',
+			'flex w-full justify-between align-center p-6 shadow-lg hover:shadow-xl rounded-lg text-[hsl(39,100%,56%)] bg-[hsla(39,100%,56%,0.15)] hover:scale-[0.98] transition duration-500 ease-in-out',
 	},
 	{
 		category: 'Verbal',
 		score: 61,
 		icon: verbal,
 		className:
-			'flex w-full justify-between align-center p-6 shadow-lg hover:shadow-xl rounded-lg text-[hsl(166,100%,37%)] bg-[hsla(166,100%,37%,0.15)]',
+			'flex w-full justify-between align-center p-6 shadow-lg hover:shadow-xl rounded-lg text-[hsl(166,100%,37%)] bg-[hsla(166,100%,37%,0.15)] hover:scale-[0.98] transition duration-500 ease-in-out',
 	},
 	{
 		category: 'Visual',
 		score: 72,
 		icon: visual,
 		className:
-			'flex w-full justify-between align-center p-6 shadow-lg hover:shadow-xl rounded-lg text-[hsl(234,85%,45%)] bg-[hsla(234,65%,45%,0.15)]',
+			'flex w-full justify-between align-center p-6 shadow-lg hover:shadow-xl rounded-lg text-[hsl(234,85%,45%)] bg-[hsla(234,65%,45%,0.15)] hover:scale-[0.98] transition duration-500 ease-in-out',
 	},
 ];
 
