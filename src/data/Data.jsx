085e6041f1b14a2f29fc4a6a19df14f251dc8284
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-05-01 17:56:56 +1200

    added hover classes to the card items

--- src/data/Data.jsx
+++ src/data/Data.jsx
@@ -9,28 +9,28 @@ const data = [
 		score: 80,
 		icon: reaction,
 		className:
-			'flex w-full justify-between align-center p-6 shadow-lg rounded-lg text-[hsl(0,89%,65%)] bg-[hsla(0,89%,65%,0.15)]',
+			'flex w-full justify-between align-center p-6 shadow-lg hover:shadow-xl rounded-lg text-[hsl(0,89%,65%)] bg-[hsla(0,89%,65%,0.15)] ',
 	},
 	{
 		category: 'Memory',
 		score: 92,
 		icon: memory,
 		className:
-			'flex w-full justify-between align-center p-6 shadow-lg rounded-lg text-[hsl(39,100%,56%)] bg-[hsla(39,100%,56%,0.15)]',
+			'flex w-full justify-between align-center p-6 shadow-lg hover:shadow-xl rounded-lg text-[hsl(39,100%,56%)] bg-[hsla(39,100%,56%,0.15)]',
 	},
 	{
 		category: 'Verbal',
 		score: 61,
 		icon: verbal,
 		className:
-			'flex w-full justify-between align-center p-6 shadow-lg rounded-lg text-[hsl(166,100%,37%)] bg-[hsla(166,100%,37%,0.15)]',
+			'flex w-full justify-between align-center p-6 shadow-lg hover:shadow-xl rounded-lg text-[hsl(166,100%,37%)] bg-[hsla(166,100%,37%,0.15)]',
 	},
 	{
 		category: 'Visual',
 		score: 72,
 		icon: visual,
 		className:
-			'flex w-full justify-between align-center p-6 shadow-lg rounded-lg text-[hsl(234,85%,45%)] bg-[hsla(234,65%,45%,0.15)]',
+			'flex w-full justify-between align-center p-6 shadow-lg hover:shadow-xl rounded-lg text-[hsl(234,85%,45%)] bg-[hsla(234,65%,45%,0.15)]',
 	},
 ];
 
