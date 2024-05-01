c1f6f34a188c3113db60ddd06fffccd62a205952
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-05-01 17:56:04 +1200

    added some transitions to the buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Button = () => {
 	return (
 		<>
-			<button className='min-h-14 btn btn-[#303B59] btn-block grid grid-cols-1 place-items-center hover:bg-gradient-to-b from-[hsla(252,100%,67%,1)] to-[hsla(241,81%,54%,1)] 100% text-white text-lg font-normal rounded-full'>
+			<button className='min-h-14 btn btn-[#303B59] btn-block grid grid-cols-1 place-items-center hover:bg-gradient-to-b from-[hsla(252,100%,67%,1)] to-[hsla(241,81%,54%,1)] 100% text-white text-lg font-normal shadow-lg rounded-full outline-none border-none transition ease-in-out duration-300 hover:scale-[0.98] hover:shadow-2xl'>
 				Continue
 			</button>
 		</>
